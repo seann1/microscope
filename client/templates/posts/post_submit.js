0f7d7d65cb4418e03a332918de546d76fa144cfe
Sean Niesen <seanniesen@gmail.com>
2015-05-18 22:36:11 -0700

    bug fixed, working

--- client/templates/posts/post_submit.js
+++ client/templates/posts/post_submit.js
@@ -7,18 +7,15 @@ Template.postSubmit.events({
 			title: $(e.target).find('[name=title]').val()
 		};
 
-		post._id = Posts.insert(post);
-		Router.go('postPage', post);
-	}
+		Meteor.call('postInsert', post, function(error, result) {
 
-	Meteor.call('postInsert', post, function(error, result) {
+			if (error)
+				return alert(error.reason);
 
-		if (error)
-			return alert(error.reason);
-
-		if (result.postExists)
-			alert('This link has already been posted');
+			if (result.postExists)
+				alert('This link has already been posted');
 		
-		Router.go('postPage', {_id: result._id});
-	});
+			Router.go('postPage', {_id: result._id});
+		});
+	}
 });
